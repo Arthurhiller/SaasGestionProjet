b731219373e525add3d42a73063978e329a77868
Arkang67 <arthur.entrepreneuriat@hotmail.com>
2023-05-19 22:47:11 +0200

    ADD: Function testUserCreationWithPassWordRegex

--- Tests/Entity/UserTest.php
+++ Tests/Entity/UserTest.php
@@ -5,8 +5,14 @@ namespace App\Tests\Entity;
 use App\Entity\User;
 use PHPUnit\Framework\TestCase;
 
+
 class UserTest extends TestCase
 {
+    /**
+     * Function to test the existence of a user
+     *
+     * @return void
+     */
     public function testUserCreation()
     {
         // create a new user
@@ -17,5 +23,26 @@ class UserTest extends TestCase
         // Verify that the user
         $this->assertEquals('Test@exemple.com', $user->getEmail());
         $this->assertEquals('password', $user->getPassword());
+    }
+
+    /**
+     * Function to test the existence of a user password with regex pattern
+     *
+     * @return void
+     */
+    public function testUserCreationWithPasswordRegex()
+    {
+        
+        // create a new user
+        $user = new User();
+        $user->setEmail('test@exemple.com');
+        $user->setPassword('Password@123');
+
+        $this->assertEquals('test@exemple.com', $user->getEmail());
+        $this->assertEquals('Password@123', $user->getPassword());
+
+
+        $regex = "/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/";
+        $this->assertMatchesRegularExpression($regex, $user->getPassword());
     }
 }
